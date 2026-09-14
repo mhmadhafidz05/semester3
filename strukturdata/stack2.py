7643f82b05ad54d56287b51ea4cea3da56f5173f
muhammadhafidz <mhmad.hafidz14@gmail.com>
2026-09-14 17:01:48 +0700

    latihan

---
+++ strukturdata/stack2.py
@@ -0,0 +1,37 @@
+class stack:
+    def __init__(self):
+        self.data = []
+
+    def insert(self, item):
+        self.data.append(item)
+        print(f"{item} berhasil ditambahkan")
+
+    def delete(self):
+        if len(self.data):
+            print("data tidak ditemukan")
+        else:
+            item = self.data.pop()
+            print(f"{item} data telah dihapus")
+    
+    def searching(self, item):
+        if item in self.data:
+            print(f"{item} ditemukan didalam stack")
+        else:
+            print(f"{item} tidak ditemukan didalam stack")
+
+    def display(self):
+        print("isi stack", self.data)
+
+stack = stack()
+
+stack.insert("yangmuliahafis")
+stack.insert("ismailbinmail")
+stack.insert("arifyutaka")
+
+stack.display()
+
+stack.searching("arifyutaka")
+
+stack.delete()
+
+stack.display()
